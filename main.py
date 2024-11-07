ce1dd89e5da47126e7dd7d92c5b40c13a7cac2bd
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-11-07 12:10:49 -0500

    Ejercicio 5: Días de la semana

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-# Ejercicio 4: Tipo de triángulo
-lado1 = float(input("Ingrese la longitud del primer lado: "))
-lado2 = float(input("Ingrese la longitud del segundo lado: "))
-lado3 = float(input("Ingrese la longitud del tercer lado: "))
+# Ejercicio 5: Días de la semana
+dia = int(input("Ingrese el número del día de la semana: "))
 
-if lado1 == lado2 == lado3:
-    print("El triángulo es equilátero.")
-elif lado1 == lado2 or lado2 == lado3 or lado1 == lado3:
-    print("El triángulo es isósceles.")
+hoy = {
+    1: "Lunes",
+    2: "Martes",
+    3: "Miércoles",
+    4: "Jueves",
+    5: "Viernes",
+    6: "Sábado",
+    7: "Domingo"
+}
+
+if dia in hoy:
+    print(f"{hoy[dia]}")
 else:
-    print("El triángulo es escaleno.")
+    print("Número de día no válido.")
+
