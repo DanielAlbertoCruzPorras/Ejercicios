1ac957c0878ac158f1370616b3adf420e5a15077
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-11-07 12:57:55 -0500

    Exercise 11: Temperature conversion

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-# Ejercicio 10: Clasificación de notas
-nota = float(input("Ingrese la nota: "))
+# Exercise 11: Temperature conversion
+temperature = float(input("Enter the temperature: "))
+scale = input("Enter the scale (C for Celsius, F for Fahrenheit): ").upper()
 
-if 90 <= nota <= 100:
-    print("Clasificación: A")
-elif 80 <= nota <= 89:
-    print("Clasificación: B")
-elif 70 <= nota <= 79:
-    print("Clasificación: C")
-elif 60 <= nota <= 69:
-    print("Clasificación: D")
-elif 0 <= nota < 60:
-    print("Clasificación: F")
-else:
-    print("Nota no válida.")
+match scale:
+    case "C":
+        # Convert Celsius to Fahrenheit
+        converted_temp = (temperature * 9/5) + 32
+        print(f"{temperature}°C is equal to {converted_temp}°F.")
+    case "F":
+        # Convert Fahrenheit to Celsius
+        converted_temp = (temperature - 32) * 5/9
+        print(f"{temperature}°F is equal to {converted_temp}°C.")
+    case _:
+        print("Invalid scale entered. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+
 
 
 
