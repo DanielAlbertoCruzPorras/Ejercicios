1dd1412e6677e74e63021969d925853c0e72a0af
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-11-12 23:41:16 -0500

    Repetitive structures - Exercise 1

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-# Exercise 22: Triangle Classification by Angles
-angle1 = float(input("Enter the first angle of the triangle: "))
-angle2 = float(input("Enter the second angle of the triangle: "))
-angle3 = float(input("Enter the third angle of the triangle: "))
+## Repetitive Structures
+# Exercise 1: Sum of the First N Integers
+n = int(input("Enter a positive integer: "))
 
-if angle1 < 90 and angle2 < 90 and angle3 < 90:
-    triangle_type = "Acute triangle"
-elif angle1 == 90 or angle2 == 90 or angle3 == 90:
-    triangle_type = "Right triangle"
-else:
-    triangle_type = "Obtuse triangle"
+sum_total = 0
 
-print(f"The triangle is: {triangle_type}")
+for i in range(1, n + 1):
+    sum_total += i
+
+print(f"The sum of the first {n} integers is: {sum_total}")
+
